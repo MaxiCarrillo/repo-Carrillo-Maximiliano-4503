b74711eac6dc48abf195add17bc51f08131e5799
maxi1 <maxi1@DESKTOP-G09S1M5>
2022-04-08 16:49:29 -0300

    primer commit

---
+++ ProjectDemo/src/test/java/ar/edu/fi/unju/aplicacion/ProjectDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.fi.unju.aplicacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
